f5a17d7c2b202b3927ac316a85579d3360d58b8b
Diyorbek <diyorbekabdumannonov4@gmail.com>
2023-03-03 15:50:48 +0500

    small change

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import {
+  EffectComposer,
+  Bloom,
+  ChromaticAberration,
+  Scanline,
+} from "@react-three/postprocessing";
+import { BlendFunction } from "postprocessing";
+import {
+  CubeCamera,
+  Environment,
+  OrbitControls,
+  PerspectiveCamera,
+} from "@react-three/drei";
+import "./style.css";
+import { Car } from "./Car";
+import { Ground } from './Ground'
+
+function App() {
+  const [color, setColor] = useState([1, 1, 2])
+  return (
+    <Suspense fallback={null}>
+      <Canvas>
+        <OrbitControls
+          target={[0, 0.35, 0]}
+          maxPolarAngle={1.45}
+        />
+        <PerspectiveCamera makeDefault fov={50} position={[3, 2, 5]} />
+        <CubeCamera resolution={256}>
+          {(texture) => (
+            <>
+              <Environment map={texture} />
+              <Car />
+            </>
+          )}
+        </CubeCamera>
+        <Ground />
+        <spotLight
+          color={color}
+          intensity={1.5}
+          angle={0.6}
+          penumbra={0.5}
+          position={[5, 5, 0]}
+          castShadow
+          shadow-bias={-0.0001}
+        />
+        <spotLight
+          color={color}
+          intensity={2}
+          angle={0.6}
+          penumbra={0.5}
+          position={[-5, 5, 0]}
+          castShadow
+          shadow-bias={-0.0001}
+        />
+        <EffectComposer>
+          {/* <DepthOfField focusDistance={0.0035} focalLength={0.01} bokehScale={3} height={480} /> */}
+          <Bloom
+            blendFunction={BlendFunction.ADD}
+            intensity={1.3} // The bloom intensity.
+            width={300} // render width
+            height={300} // render height
+            kernelSize={5} // blur kernel size
+            luminanceThreshold={0.15} // luminance threshold. Raise this value to mask out darker elements in the scene.
+            luminanceSmoothing={0.025} // smoothness of the luminance threshold. Range is [0, 1]
+          />
+          <ChromaticAberration
+            blendFunction={BlendFunction.NORMAL} // blend mode
+            offset={[0.0005, 0.0012]} // color offset
+          />
+        </EffectComposer>
+      </Canvas>
+
+    </Suspense>
+  );
+}
+
+export default App;
---
+++ src/Car.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from "react";
+import { useLoader } from "@react-three/fiber";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { EquirectangularReflectionMapping, Mesh, MeshStandardMaterial, RepeatWrapping, Scene, TextureLoader } from "three";
+
+export function Car() {
+  const gltf = useLoader(
+    GLTFLoader,
+    process.env.PUBLIC_URL + "models/car/scene.gltf"
+  );
+  const scene = new Scene()
+
+  useEffect(() => {
+    gltf.scene.scale.set(1, 1, 1);
+    gltf.scene.position.set(0, -0.035, 0);
+    const newMaterial = new MeshStandardMaterial({ color: 0xff0000 });
+    gltf.scene.traverse((object) => {
+      console.log(object.name)
+      if (object.name === 'Object_28') {
+        object.material = newMaterial;
+      }
+      if (object instanceof Mesh) {
+        object.castShadow = true;
+        object.receiveShadow = true;
+        object.material.envMapIntensity = 20;
+      }
+    });
+  }, [gltf]);
+
+  return <primitive object={gltf.scene} />;
+}
